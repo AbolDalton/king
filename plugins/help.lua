2c8eb2def4647e46dc91c8e5c1b3c1c582502d8d
Abolfazl Dalton <abolfazl.k.Dalton@gmail.com>
2017-06-29 11:00:52 +0000

    Create help.lua

---
+++ plugins/help.lua
@@ -0,0 +1,110 @@
+do
+
+function run(msg, matches)
+if matches[1]:lower() == 'help' then
+   send_document(get_receiver(msg), "راهنمای سوپر گروه  :
+
+🔵راهنمای مگا بات🔴
+
+
+!admins
+🔸لیست ادمین های گروه
+
+!owner
+🔸آیدی صاحب گروه
+
+!modlist
+🔸لیست مدیران گروه
+
+!bots
+🔸لیست ربات های داخلروه
+
+!who
+🔸لیست کاربران داخل  گروه
+
+!block
+🔸اخراج کردن فرد از گروه با ریپلی
+
+!ban
+🔸بن کردن فرد از گروه
+
+!unban
+🔸حذف بن کردن فرد بن شده
+
+!setowner
+🔸انتخاب صاحب گروه
+
+!promote 
+🔸انتخاب فرد به عنوان مدیر
+
+!demote
+🔸ازل مقام یک مدیر
+
+!setadmin
+🔸اتتخاب فرد به عنوان ادمین
+
+!demoteadmin
+🔸ازل مقام فرد ادمین
+
+!setname/photo/rules/about
+🔹تنظیم اسم/عکس/قوانین/موضوع گروه
+
+!newlink
+🔹ساخت لینک جدید
+
+!link
+🔹دریافت لینک گروه
+
+!rules
+🔹دریافت قوانین گروه
+
+!lock links/flood/spam/arabic/member/rtl/sticker/fwd/all
+
+🔹قفل کردن گذینه های بالا
+
+!unlock ****
+🔹باز کردن گذینه های قفل شده
+
+!addword [word]/rw [word]/badwords
+🔺فیلتر کلمه/حذف کلمه فیلتر شده/لیست کلمات فیلتر
+
+!mute all/audio/gifs/photo/video/service/text
+🔺ممنوع کردن همه/صدا/گیف/عکس/ویدئو/سرویس/متن
+
+!unmute ****
+🔺آزاد کردن گذینه های ممنوع شده
+
+!setflood [value]
+🔺تنظیم حساسیت به اسپم (عددی بین 1 الی 200)
+
+!settings
+🔻دریافت تنظیمات گروه
+
+!muteslist
+🔻دریافت لیست ممنوع شده ها
+
+!banlist
+▪️لیست افراد بن شده
+
+!clean rules/about/modlist
+▪️پاک کردن لیست قوانین/موضوع/مدیران
+
+!del
+▪️حذف پیام از گروه با ریپلی
+
+!public yes/no
+▪️تنظیم عمومی کردن گروه یا برعکس
+
+!res @username
+▪️دریافت ایدی و اسم فردی که یوزرش وارد شده", ok_cb, false)
+end
+end
+return {
+patterns = {
+"^[!#/][help]$",
+
+},
+run = run
+}
+
+end
